4a7afe11f77190ac1e8a092f23ce79a107986dfb
David Melendez <melenava@gmail.com>
2025-10-06 23:04:19 -0600

    npx prettier --write scripts-ts/deploy.ts

--- packages/snfoundry/scripts-ts/deploy.ts
+++ packages/snfoundry/scripts-ts/deploy.ts
@@ -59,7 +59,8 @@ const deployScript = async (): Promise<void> => {
 
 const deployScript = async (): Promise<void> => {
   // Dirección del VRF provider de Cartridge en Sepolia testnet
-  const VRF_PROVIDER_ADDRESS = "0x051fea4450da9d6aee758bdeba88b2f665bcbf549d2c61421aa724e9ac0ced8f";
+  const VRF_PROVIDER_ADDRESS =
+    "0x051fea4450da9d6aee758bdeba88b2f665bcbf549d2c61421aa724e9ac0ced8f";
 
   await deployContract({
     contract: "Randomness",
@@ -70,7 +71,7 @@ const deployScript = async (): Promise<void> => {
     },
   });
 };
- 
+
 const main = async (): Promise<void> => {
   try {
     await deployScript();
